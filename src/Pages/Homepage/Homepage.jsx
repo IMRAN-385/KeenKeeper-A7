c263fba5cdefb10e305aaae337e928a3d7f09bb5
IMRAN-385 <imranhimel385@gmail.com>
2026-04-17 11:26:36 +0600

    finished homepage banner

---
+++ src/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import friendsData from '../../../data.json'
+import Card from './Card'
+
+const Homepage = () => {
+  const [friends, setFriends] = useState([])
+  const [loading, setLoading] = useState(true)
+  const total = friends.length
+  const onTrack = friends.filter(f => f.status === "on-track").length
+  const needAttention = friends.filter(f => f.status === "overdue" || f.status === "almost due").length
+  const interactions = 12
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFriends(friendsData)
+      setLoading(false)
+    }, 1500)
+  }, [])
+
+  if (loading) return <div className="flex justify-center p-20">Loading...</div>
+
+  return (
+    <div className="p-10">
+      <h2 className="text-center text-3xl font-bold">Friends to keep close in your life</h2>
+      <p className="text-center text-gray-400 text-sm mt-2">
+        Your personal shelf of meaningful connections.
+      </p>
+      <div className="flex justify-center mt-4">
+        <button className="bg-green-700 text-white px-4 py-2 rounded-lg">+ Add a Friend</button>
+      </div>
+
+      <h3 className="text-xl font-semibold mt-10 mb-4">Your Friends</h3>
+      
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 mt-6 pb-6">
+          <div className="bg-white rounded-xl p-4 text-center shadow-sm">
+            <h3 className="text-2xl font-bold">{total}</h3>
+            <p className="text-gray-400 text-sm">Total Friends</p>
+          </div>
+          <div className="bg-white rounded-xl p-4 text-center shadow-sm">
+            <h3 className="text-2xl font-bold">{onTrack}</h3>
+            <p className="text-gray-400 text-sm">On Track</p>
+          </div>
+          <div className="bg-white rounded-xl p-4 text-center shadow-sm">
+            <h3 className="text-2xl font-bold">{needAttention}</h3>
+            <p className="text-gray-400 text-sm">Need Attention</p>
+          </div>
+          <div className="bg-white rounded-xl p-4 text-center shadow-sm">
+            <h3 className="text-2xl font-bold">{interactions}</h3>
+            <p className="text-gray-400 text-sm">Interactions This Month</p>
+          </div>
+        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {friends.map(friend => (
+          <Card key={friend.id} friend={friend} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { Component, StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { RouterProvider } from 'react-router/dom'
+import { Component, StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { RouterProvider } from 'react-router/dom';
 import { createBrowserRouter} from "react-router";
 import Rootlayout from "./layouts/Rootlayout";
-import Homepage from './Pages/Homepage/Homepage.jsx'
-import Friends from './Pages/Friends.jsx'
-import Timeline from './Pages/Timeline.jsx'
-import Stats from './Pages/Stats.jsx'
+import Homepage from './Pages/Homepage/Homepage.jsx';
+import Friends from './Pages/FriendDetail.jsx';
+import Timeline from './Pages/Timeline.jsx';
+import Stats from './Pages/Stats.jsx';
+import FriendDetail from './Pages/FriendDetail.jsx'
+import { Toaster } from 'react-hot-toast';
+
 
 
 
@@ -24,6 +27,10 @@ const router = createBrowserRouter([
       {
         path: "friends",
         element: <Friends/>,
+      },
+      {
+        path: "friends/:id",
+        element: <FriendDetail/>,
       },
       {
         path: "timeline",
@@ -45,6 +52,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <RouterProvider router={router} />
-      
+      <Toaster position="top-right" />
+
   </StrictMode>,
 )
